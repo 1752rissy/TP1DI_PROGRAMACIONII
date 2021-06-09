5d0ebe5a79a6345db69032b997fa7dd32f1e7e75
Andrés Gutierrez <agutierrez@microtrack.com.ar>
2021-06-09 18:44:32 -0300

    primer commit a master

---
+++ DITP1Agutierrez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITP1Agutierrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primer commit 
+        }
+    }
+}
